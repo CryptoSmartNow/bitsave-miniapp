9d2d3157505b033027198cae4362c32736f5bb95
Emmo00 <nwaforemmanuel005@gmail.com>
2025-08-19 20:16:52 +0100

    feat(miniapp): Add farcaster embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import HowItWorks from './components/HowItWorks';
@@ -6,6 +7,24 @@ import Features from './components/Features';
 import Team from './components/Team';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
+import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "../lib/constants";
+import { getMiniAppEmbedMetadata } from "../lib/utils";
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: APP_NAME,
+    openGraph: {
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+      images: [APP_OG_IMAGE_URL],
+    },
+    other: {
+      "fc:frame": JSON.stringify(getMiniAppEmbedMetadata()),
+    },
+  };
+}
 
 export default function Home() {
   return (
