a40a77496c936223700a9416ff165a96a3de7b07
Emmo00 <nwaforemmanuel005@gmail.com>
2025-09-02 19:45:26 +0100

    chore: update @neynar/react to version 1.2.5 and next to version 15.5.2 in package.json

--- components/CustomConnectButton.tsx
+++ components/CustomConnectButton.tsx
@@ -2,7 +2,7 @@
 
 import farcasterMiniApp from "@farcaster/miniapp-wagmi-connector";
 import { useConnect, useAccount } from "wagmi";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function CustomConnectButton() {
   const { connect } = useConnect();
