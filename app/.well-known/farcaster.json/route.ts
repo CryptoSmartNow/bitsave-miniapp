bd06dc832d67ad54c3919d3ffad934c91a0f3935
Emmo00 <nwaforemmanuel005@gmail.com>
2025-09-02 22:27:42 +0100

    ...

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-import { getFarcasterDomainManifest } from "@/lib/utils";
+// import { getFarcasterDomainManifest } from "@/lib/utils";
 
 export async function GET() {
   try {
     return NextResponse.json({ hi: "world" });
-    const config = await getFarcasterDomainManifest();
-    return NextResponse.json(config);
+    // const config = await getFarcasterDomainManifest();
+    // return NextResponse.json(config);
   } catch (error) {
     console.error("Error generating metadata:", error);
     const errorMessage = error instanceof Error ? error.message : "Unknown error occurred";
