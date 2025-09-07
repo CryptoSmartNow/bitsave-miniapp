a86c5f9d05d666649cd500eb981efc279f744eb3
Emmo00 <nwaforemmanuel005@gmail.com>
2025-09-07 22:10:06 +0100

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
 "use client";
 
-import HomePage from "../components/HomePage";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import HowItWorks from "../components/HowItWorks";
+import Security from "../components/Security";
+import Features from "../components/Features";
+import Team from "../components/Team";
+import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
 
 export default function Home() {
-  return <HomePage />;
+  useEffect(() => {
+    sdk.actions.ready();
+    console.log("ready Bitsave.io");
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <HowItWorks />
+        <Security />
+        <Features />
+        <Team />
+        <FAQ />
+      </main>
+      <Footer />
+    </div>
+  );
 }
