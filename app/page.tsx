73ab7441035618ae382ad483ee680bf4866631f4
Emmo00 <nwaforemmanuel005@gmail.com>
2026-01-19 19:12:14 +0100

    feat: Add Head component to Home for meta tag inclusion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import Dashboard from "./dashboard/page";
+import Head from "next/head";
 
 export default function Home() {
-  return <Dashboard />;
+  return (
+    <>
+      <Head>
+        <meta name="base:app_id" content="696e6dbbf22fe462e74c158f" />
+      </Head>
+      <Dashboard />
+    </>
+  );
 }
